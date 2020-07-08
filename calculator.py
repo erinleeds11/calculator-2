067e3d31207c6b2d60b1e3e2e702e853c1b6e7bc
Erin Leeds <erin@erinleeds.com>
2020-07-08 22:43:59 +0000

    Assigned variables

--- calculator.py
+++ calculator.py
@@ -4,4 +4,15 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+while True:
+    user_equation = input("Enter an equation > ")
+    tokens = user_equation.split("")
+    if tokens[0] == 'q':
+        print("Quit.")
+        break
+    else:
+        function = tokens[0]
+        num1 = tokens[1]
+        if len(tokens) == 3:
+            num2 = tokens[2]
+
